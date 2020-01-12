66c00f0904067b1a2b3e0d9efe185ebc5b946640
Andor Saga <andor.saga@gmail.com>
2020-01-12 17:37:38 -0500

    .

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-window._test = function(){
-	console.log('test');
+let Controls, controls;
+let gui;
+
+
+window._test = function() {
+  console.log('test');
+}
+
+Controls = function() {
+  this.saveFrame = function() {
+    console.log('save frame');
+    save();
+  }
+  this.recordVideo = function() {
+    console.log('record video');
+  };
+};
+
+function initCapture() {
+
+  controls = new Controls();
+  gui = new dat.GUI();
+  gui.add(controls, 'saveFrame');
+  gui.add(controls, 'recordVideo');
+
+  cap = new CCapture({ format: 'webm', verbose: true, framerate: 30, name: 'test' });
 }
