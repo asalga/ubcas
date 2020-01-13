e0e53b3c36797d003f057c492b3df1f6aae89eed
Andor Saga <andor.saga@gmail.com>
2020-01-12 20:21:52 -0500

    .

--- index.js
+++ index.js
@@ -7,26 +7,40 @@ let _p5;
 const DEBUG = true;
 
 let Controls, controls;
+let playPauseLabel;
 let gui;
 
 let cap;
 let isRecording = false;
 let savedFrames = 0;
-let framesToSave = 100;
-		
+let framesToSave = 50;
+let isPlaying = true;
+
+
 Controls = function() {
-  this.saveFrame = () => save()
+  this.saveFrame = function() { save(); }
 
   this.recordVideo = function() {
-    console.log('record video');
     cap.start();
     isRecording = true;
   };
+
+  this.playPause = function() {
+    isPlaying = !isPlaying;
+
+    if (isPlaying) {
+      playPauseLabel.name('playing');
+      loop();
+    } else {
+      playPauseLabel.name('paused');
+      noLoop();
+    }
+  }
 };
 
 function saveVideoFrame() {
   if (isRecording === false) return;
-	
+
   if (savedFrames === framesToSave) {
     isRecording = false;
     savedFrames = 0;
@@ -39,12 +53,16 @@ function saveVideoFrame() {
   savedFrames++;
 }
 
-function initCapture() {
-
+function initControls() {
   controls = new Controls();
+  window.controls = controls;
   gui = new dat.GUI();
-  gui.add(controls, 'saveFrame');
-  gui.add(controls, 'recordVideo');
+  gui.add(controls, 'saveFrame')
+    .name('save frame');
+  gui.add(controls, 'recordVideo')
+    .name('record video');
+  playPauseLabel = gui.add(controls, 'playPause')
+    .name('playing');
 
   cap = new CCapture({ format: 'webm', verbose: true, framerate: 30, name: 'test' });
 }
